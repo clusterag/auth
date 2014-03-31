4f6e5518fa84653e5ae6bed037c1dacd7f22aa18
vawa143 <vawa143@debian>
2014-03-31 18:46:36 +0200

    added basic login functionality

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
 
+$username = $_POST["username"];  //Benutzer
+$password = $_POST["password"];  //Passwort
+
+$dbp = "sGN59LNdKJJScrTK"  // passwort der Datenbank
+
+$database = new mysqli("db521844234.db.1and1.com", "dbo521844234", $dbp, "db521844234" );  //connect to database
+
+$hash_query = "SELECT PW FROM users WHERE UID =".$username;
+$hash = $database->query($hash_query);
+
+$logged_in = password_verify($password, $hash);
+
+if $logged_in{
+	//redirect
+}
+
 ?>
