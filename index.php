fa6d8770d97036987fd7ea59b2d0af18e683e7db
John Eberle <john@clusterag.leiningergymnasium.de>
2014-04-02 12:40:45 +0200

    echo

--- index.php
+++ index.php
@@ -22,6 +22,7 @@ else {
 $database = new mysqli("db521844234.db.1and1.com", "dbo521844234", $dbp, "db521844234" );  //connect to database
 
 $hash_query = "SELECT PW FROM users WHERE UID = '" . $username . "'";
+echo($hash_query);
 $hash = $database->query($hash_query);
 
 echo($hash);
