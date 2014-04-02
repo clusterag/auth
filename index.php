bf839cf43460f7757cba51294df5e7bc619d2c08
John Eberle <john@clusterag.leiningergymnasium.de>
2014-04-02 12:29:39 +0200

    echo

--- index.php
+++ index.php
@@ -1,12 +1,27 @@
 <?php
+//CONF
+$login_template_path = "/kunden/homepages/34/d446716986/htdocs/vertretungsplan_backend/auth/login.html";
+$dbp = "sGN59LNdKJJScrTK";  // passwort der Datenbank
+$error_not_logged_in = "Benutzername oder Passwort sind falsch. Bitte versuchen Sie es erneut.";
 
-$login_template = file_get_contents("/kunden/homepages/34/d446716986/htdocs/vertretungsplan_backend/auth/login.html");
-echo($login_template);
+$login_template = file_get_contents($login_template_path);
 
 $username = $_POST["username"];  //Benutzer
 $password = $_POST["password"];  //Passwort
 
-$dbp = "sGN59LNdKJJScrTK";  // passwort der Datenbank
+if (isset($username)) {
+	echo($error_not_logged_in);
+	echo($login_template);
+}
+else {
+	echo($login_template);
+}
+
+
+
+
+
+
 
 $database = new mysqli("db521844234.db.1and1.com", "dbo521844234", $dbp, "db521844234" );  //connect to database
 
