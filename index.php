8d336319053d48da8109168eb5ef98e549aeb946
John Eberle <john@clusterag.leiningergymnasium.de>
2014-04-02 12:34:19 +0200

    echo

--- index.php
+++ index.php
@@ -5,27 +5,23 @@ $dbp = "sGN59LNdKJJScrTK";  // passwort der Datenbank
 $error_not_logged_in = "Benutzername oder Passwort sind falsch. Bitte versuchen Sie es erneut.";
 
 $login_template = file_get_contents($login_template_path);
-
 $username = $_POST["username"];  //Benutzer
 $password = $_POST["password"];  //Passwort
 
+//if a username has been posted, i.e. user is trying to login
 if (isset($username)) {
 	echo($error_not_logged_in);
 	echo($login_template);
 }
+//if no username has been posted, i.e. user has not tried to login yet
 else {
 	echo($login_template);
 }
 
 
-
-
-
-
-
 $database = new mysqli("db521844234.db.1and1.com", "dbo521844234", $dbp, "db521844234" );  //connect to database
 
-$hash_query = "SELECT PW FROM users WHERE UID = ".$username;
+$hash_query = "SELECT PW FROM 'users' WHERE UID = ".$username;
 $hash = $database->query($hash_query);
 
 echo($hash);
