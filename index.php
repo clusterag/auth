5d72032f5abcc9e299deab8a84e118e05ce66f20
John Eberle <john@clusterag.leiningergymnasium.de>
2014-04-03 23:41:32 +0200

    index

--- index.php
+++ index.php
@@ -17,37 +17,67 @@ $morgen = $root .  "output/morgen.html";
 //	};
 //};
 
-session_start();
-
-
-$username = $_POST["username"];  //Benutzer
-$password = $_POST["password"];  //Passwort
-
-
-
-
-$database = new mysqli("db521844234.db.1and1.com", "dbo521844234", $dbp, "db521844234" );  //connect to database
-
-$hash_query = "SELECT PW FROM users WHERE UID = '" . $username . "'";
-// echo($hash_query);
-$hash = mysqli_fetch_assoc($database->query($hash_query))["PW"];
-
-
-$_SESSION["logged_in"] = password_verify($password, $hash);
-
-if ($_SESSION["logged_in"]) {
-	echo (file_get_contents($heute));
-}
-else {
-	//if a username has been posted, i.e. user is trying to login
-	if (isset($username)) {
-		echo($error_not_logged_in);
-		echo($login_template);
+function session_logged_in(){
+	if (isset($SESSION["username")]){
+		return True;
 	}
-	//if no username has been posted, i.e. user has not tried to login yet
 	else {
-		echo($login_template);
+		return False;
 	}
 }
+
+function check_password($username, $password){
+	$database = new mysqli("db521844234.db.1and1.com", "dbo521844234", $dbp, "db521844234" );  //connect to database
+	$hash_query = "SELECT PW FROM users WHERE UID = '" . $username . "'";
+	// echo($hash_query);
+	$hash = mysqli_fetch_assoc($database->query($hash_query))["PW"];
+	//AFAIK, storing boolean in cookies is not advisable
+	$logged_in = password_verify($password, $hash);
+	return $logged_in;
+}
+
+function check_login(){
+	//checks if the session is logged in
+	//if it is not, gets POST params and checks password
+	//if password is wrong or none given echoes login template
+
+	session_set_cookie_params(300);
+	session_start();
+	//if session is logged in return True
+	if(session_logged_in()){
+		return True;
+	}
+	//if session is not logged in
+	else {
+		//getting POST parameters
+		$username = $_POST["username"];  //Benutzer
+		$password = $_POST["password"];  //Passwort
+		
+		//if a username has been posted, i.e. user is trying to login
+		if (isset($username)) {
+			//checking password
+			//if password is correct, set username in session to username, return true
+			if(check_password($username, $password)){
+				$_SESSION["username"] = $username;
+				return True;
+			}
+			else {
+				return False;
+				echo($error_not_logged_in);
+				echo($login_template);
+			};
+		}
+		
+		//if no username has been posted, i.e. user has not tried to login yet
+		else {
+			return False;
+			echo($login_template);
+		};
+	};
+}
+//check_login must be the first function called because it sets the session cookie which must be done before the http body starts
+if (check_login()) {
+	echo (file_get_contents($heute));
+};
 
 ?>
