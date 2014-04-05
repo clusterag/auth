9156e07700c959c2076c5d52fbb33ad5b4a203ac
John Eberle <john@clusterag.leiningergymnasium.de>
2014-04-05 16:27:16 +0200

    refactored_code_moved_into_functions_sessions_are_working_properly_now

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ $morgen = $root .  "output/morgen.html";
 //};
 
 function session_logged_in(){
-	if (isset($SESSION["username")]){
+	if ($_SESSION["logged_in"] == 1){
 		return True;
 	}
 	else {
@@ -26,23 +26,23 @@ function session_logged_in(){
 	}
 }
 
-function check_password($username, $password){
+function check_password($username, $password, $dbp){
 	$database = new mysqli("db521844234.db.1and1.com", "dbo521844234", $dbp, "db521844234" );  //connect to database
 	$hash_query = "SELECT PW FROM users WHERE UID = '" . $username . "'";
 	// echo($hash_query);
 	$hash = mysqli_fetch_assoc($database->query($hash_query))["PW"];
-	//AFAIK, storing boolean in cookies is not advisable
 	$logged_in = password_verify($password, $hash);
 	return $logged_in;
 }
 
-function check_login(){
+function check_login($login_template_path, $error_not_logged_in, $dbp){
 	//checks if the session is logged in
 	//if it is not, gets POST params and checks password
 	//if password is wrong or none given echoes login template
 
 	session_set_cookie_params(300);
 	session_start();
+	echo($_SESSION["username"]);
 	//if session is logged in return True
 	if(session_logged_in()){
 		return True;
@@ -53,33 +53,42 @@ function check_login(){
 		$username = $_POST["username"];  //Benutzer
 		$password = $_POST["password"];  //Passwort
 		$login_template = file_get_contents($login_template_path);
-		
+		#echo($login_template);
+		#echo($username);
 		//if a username has been posted, i.e. user is trying to login
-		if (isset($username)) {
+		if (isset($_POST["username"])) {
 			//checking password
 			//if password is correct, set username in session to username, return true
-			if(check_password($username, $password)){
+			if(check_password($username, $password, $dbp)){
 				$_SESSION["username"] = $username;
+				$_SESSION["logged_in"] = 1;
 				return True;
 			}
 			else {
-				return False;
 				echo($error_not_logged_in);
 				echo($login_template);
+				return False;
 			};
 		}
-		
 		//if no username has been posted, i.e. user has not tried to login yet
 		else {
-			return False;
 			echo($login_template);
+			return False;
+			
 		};
 	};
 }
 //check_login must be the first function called because it sets the session cookie which must be done before the http body starts
-if (check_login()) {
-	#echo (file_get_contents($heute));
+if (check_login($login_template_path, $error_not_logged_in, $dbp)) {
+	if($_GET["pid"] == 1){
+		echo (file_get_contents($morgen));
+	}
+	else {
+		echo (file_get_contents($heute));
+	};
+	echo($_GET["pid"]);
 	echo("success");
+	echo($_SESSION["username"]);
 };
 
 ?>
