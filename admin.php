e9c492018b04aa0e90b8766ec224add325d2bfea
John Eberle <john@clusterag.leiningergymnasium.de>
2014-04-07 16:33:03 +0200

    conf

--- admin.php
+++ admin.php
@@ -1,7 +1,9 @@
 <?php
 include "index.php";
+if (login($login_template_path, $error_not_logged_in, $dbhost, $dbuser, $dbp, $db_database, "users")) {
+	echo(file_get_contents($admin_template_path));
 
-login($login_template_path, $error_not_logged_in, $db_host, $db_user,$dbp, $db, $table);
+}
 
 
 ?>
