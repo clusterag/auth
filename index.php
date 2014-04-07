514c412b200785e8e2e7d6d4f53e0e9494fb464d
vawa143 <vawa143@debian>
2014-04-07 10:31:39 +0200

    dbp

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $login_template_path = $root . "auth/login.html";
 $logout_template_path = $root . "auth/logout.html";
 #$dbp = "";  // passwort der Datenbank
 $dbp = file_get_contents($root . "dbp");
-echo ($root . "dbp");
+echo ($dbp);
 $error_not_logged_in = "Benutzername oder Passwort sind falsch. Bitte versuchen Sie es erneut.";
 $heute = $root . "auth/heute.html";
 $morgen = $root .  "auth/morgen.html";
