2e8da0847061ece020573a8a3187fe10df326098
John Eberle <john@clusterag.leiningergymnasium.de>
2014-04-07 15:04:36 +0200

    conf

--- conf.php
+++ conf.php
@@ -4,7 +4,6 @@ $login_template_path = $root . "auth/login.html";
 $logout_template_path = $root . "auth/logout.html";
 $dbp = "";  // passwort der Datenbank
 $dbp = file_get_contents($root . "dbp");
-
 $error_not_logged_in = "Benutzername oder Passwort sind falsch. Bitte versuchen Sie es erneut.";
 $heute = $root . "auth/heute.html";
 $morgen = $root .  "auth/morgen.html";
