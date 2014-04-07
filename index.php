205bb3e31de5c9a2b3282bbadabfe21776648d58
vawa143 <vawa143@debian>
2014-04-07 11:09:45 +0200

    dbp

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php
 //CONF
-$root = "/kunden/homepages/34/d446716986/htdocs/vertretungsplan_backend/";
-$login_template_path = $root . "auth/login.html";
-$logout_template_path = $root . "auth/logout.html";
+$conf = "/kunden/homepages/34/d446716986/htdocs/vertretungsplan_backend/conf.php" ;
+
+#$root = "";
+#$login_template_path = $root . "auth/login.html";
+#$logout_template_path = $root . "auth/logout.html";
 #$dbp = "";  // passwort der Datenbank
-$dbp = file_get_contents($root . "dbp");
+#$dbp = file_get_contents($root . "dbp");
+
 $error_not_logged_in = "Benutzername oder Passwort sind falsch. Bitte versuchen Sie es erneut.";
 $heute = $root . "auth/heute.html";
 $morgen = $root .  "auth/morgen.html";
