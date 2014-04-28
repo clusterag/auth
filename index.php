513fd0f2e08e19590221d222a63253989842dab0
John Eberle <john@clusterag.leiningergymnasium.de>
2014-04-28 16:03:28 +0200

    db

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include $conf;
 include "functions.php";
 
 //login must be the first function called because it sets the session cookie which must be done before the http body starts
-if (login($login_template, $error_not_logged_in, $db_host, $db_user, $db_password, $db_database, "users") && $_SERVER["REQUEST_URI"] != "/auth/admin.php") {
+if (login($login_template, $error_not_logged_in) && $_SERVER["REQUEST_URI"] != "/auth/admin.php") {
 	if($_GET["pid"] == 1){
 		echo (file_get_contents($morgen));
 	}
