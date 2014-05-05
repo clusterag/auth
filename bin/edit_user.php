b42ba73def43dd835441c17efeb77b7e36da2388
John Eberle <john@clusterag.leiningergymnasium.de>
2014-05-05 17:33:27 +0200

    add_user_edit_user

--- bin/edit_user.php
+++ bin/edit_user.php
@@ -2,7 +2,7 @@
 include "functions.php";
 
 if (login() && is_admin()) {
-	echo(make_html(True, file_get_contents($add_user_template_path)));
+	echo(make_html(True, file_get_contents($edit_user_template_path)));
 
 }
 
