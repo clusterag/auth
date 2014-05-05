1d74dbce856525d39daa4d8936c6887a242a2f0f
John Eberle <john@clusterag.leiningergymnasium.de>
2014-05-05 16:23:03 +0200

    settings

--- bin/settings.php
+++ bin/settings.php
@@ -9,6 +9,9 @@ if (login()){
 		elseif (!($_POST["password_1"] == $_POST["password_2"])){
 			echo (make_html(True, "Neue Passworter stimmen nicht uberein." . file_get_contents($settings_template_path)));
 		}
+		else {
+			set_password($_SESSION["username"], $_POST["password_1"]);
+		}
 	}
 	echo (make_html(True, file_get_contents($settings_template_path)));
 }
