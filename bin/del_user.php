d06de42391de4e359cb25ad56e0ecd91de145a4a
Valentin Wagner <vwagner@clusterag.leiningergymnasium.de>
2014-05-09 20:38:10 +0200

    users

--- bin/del_user.php
+++ bin/del_user.php
@@ -7,6 +7,7 @@ if (login() && is_admin()) {
 	//if ($username && is_user($username) && $username !="admin"){
 	if ($username && $username != "admin"){
 			del_user($username);
+			header('Location: admin.php');
 	}
 	elseif ($username){
 		echo(make_html(True, "Benutzer existiert nicht." . file_get_contents($del_user_template_path)));
