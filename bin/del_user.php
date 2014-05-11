df217e1fdc032565bfe5c774e95f22bc1c097074
John Eberle <john@clusterag.leiningergymnasium.de>
2014-05-11 18:42:37 +0200

    fixed #3

--- bin/del_user.php
+++ bin/del_user.php
@@ -6,7 +6,7 @@ $username = $_POST["username"];
 if (login() && is_admin() ) {
 	if ($username && $username != "admin"){
 			del_user($username);
-			echo(make_html(True, "Benutzer \"" . $username . "\" gelöscht" . file_get_contents($del_user_template_path)));
+			echo(make_html(True, "Benutzer \"" . $username . "\" gel&ouml;scht" . file_get_contents($del_user_template_path)));
 	}
 	elseif ($username){
 		echo(make_html(True, "Benutzer \"" . $username . "\" existiert nicht." . file_get_contents($del_user_template_path)));
--- bin/settings.php
+++ bin/settings.php
@@ -7,11 +7,11 @@ if (login()){
 			echo (make_html(True, "Altes Passwort stimmt nicht." . file_get_contents($settings_template_path)));
 		}
 		elseif (!($_POST["password_1"] == $_POST["password_2"])){
-			echo (make_html(True, "Neue Passworter stimmen nicht uberein." . file_get_contents($settings_template_path)));
+			echo (make_html(True, "Neue Passw&ouml;rter stimmen nicht uberein." . file_get_contents($settings_template_path)));
 		}
 		else {
 			set_password($_SESSION["username"], $_POST["password_1"]);
-			echo (make_html(True, "Passwort erfolgreich geandert." . file_get_contents($settings_template_path)));
+			echo (make_html(True, "Passwort erfolgreich ge&auml;ndert." . file_get_contents($settings_template_path)));
 		}
 	}
 	else {
