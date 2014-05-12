9975f89961894041cf2e86489e278010b4585f47
John Eberle <john@clusterag.leiningergymnasium.de>
2014-05-12 15:10:40 +0200

    try

--- bin/functions.php
+++ bin/functions.php
@@ -176,8 +176,8 @@ function is_user($username){
 
 	// this always returns True
 	// SELECT * FROM `users` WHERE `UID`='$username';
-	//$query = "SELECT * FROM `users` WHERE `UID`='" . $username . "';";
-	$query = "SELECT 1 FROM 'users' WHERE 'UID' = '" . $username . "';";
+	$query = "SELECT * FROM `users` WHERE `UID`='" . $username . "';";
+	//$query = "SELECT 1 FROM 'users' WHERE 'UID' = '" . $username . "';";
 	$result = $database->query($query);
 	echo ($result->num_rows);
 	//if (){
