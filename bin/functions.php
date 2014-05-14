5b94f06cb1ca93bce9cab250a5b3fea8e99a5b19
John Eberle <john@clusterag.leiningergymnasium.de>
2014-05-14 19:23:04 +0200

    user_list

--- bin/functions.php
+++ bin/functions.php
@@ -129,7 +129,7 @@ function db_set_field($database, $table, $set_field, $set_value, $where_field, $
 function get_user_list(){
 	$database = db_connect();
 	$query = "SELECT * FROM `users` LIMIT 5;";
-	$list = $database->query($query);
+	$list = mysqli_fetch_assoc($database->query($query));
 	foreach ($list as $item){
 		echo ($item);
 	}
