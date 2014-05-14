3dd49606429befd6437aee78779f04d6043a7162
John Eberle <john@clusterag.leiningergymnasium.de>
2014-05-14 19:40:23 +0200

    user_list

--- bin/functions.php
+++ bin/functions.php
@@ -134,7 +134,7 @@ function get_user_list(){
 	foreach ($list as $item){
 		//$users = $users . $subitem . "\r";
 		$item = mysqli_fetch_assoc($item);
-		echo $item;
+		echo (var_dump($item));
 	//	foreach($item as $subitem){
 	//		$users = $users . $subitem . "\n";
 	//	}
