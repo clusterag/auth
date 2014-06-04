1b64f44ea9ed56d6367cad82347d0f8a3a9073f8
Valentin Wagner <vwagner@clusterag.leiningergymnasium.de>
2014-06-04 17:09:49 +0200

    gen_passwd

--- bin/functions.php
+++ bin/functions.php
@@ -14,7 +14,7 @@ function gen_passwd(){
 	$password = "";
 	$letters = array("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F");
 	for ($i=0; $i < 6; $i++) { 
-		$password = $password . array_rand($letters);
+		$password = $password . $letters[array_rand($letters)];
 	}
 	return $password;
 }
