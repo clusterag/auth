eb30cfe8d65f7e98b159c277b42630cc2fc5e54b
John Eberle <john@clusterag.leiningergymnasium.de>
2014-07-07 23:22:43 +0200

    log

--- bin/list.php
+++ bin/list.php
@@ -17,6 +17,7 @@ include "functions.php";
 if (login() && is_admin() && $_SESSION["username"] == "admin" ) {
 
 	echo(make_html(True, "THIS FILE TEMPORARILY CONTAINS log.php" . file_get_contents($log_path)));
+	file_put_contents($log_path, "test", FILE_APPEND );
 
 }
 
