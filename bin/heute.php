fd51c5ac8c4bafa766e8df969c20ff2688c459cd
John Eberle <john@clusterag.leiningergymnasium.de>
2014-08-23 13:11:38 +0200

    added dead man switch variable  in conf. If set to True, unprocessed uploaded html will be output to user.

--- bin/heute.php
+++ bin/heute.php
@@ -4,7 +4,12 @@ include "functions.php";
 
 //login must be the first function called because it sets the session cookie which must be done before the http body starts
 if (login()) {
+	if ($disable_python) {
+		echo (make_html(True, file_get_contents($heute_raw)));
+	}
+	else {
 		echo (make_html(True, file_get_contents($heute)));
+	}
 };
 
 
--- bin/morgen.php
+++ bin/morgen.php
@@ -2,9 +2,13 @@
 //CONF
 include "functions.php";
 
-//login must be the first function called because it sets the session cookie which must be done before the http body starts
 if (login()) {
+	if ($disable_python) {
+		echo (make_html(True, file_get_contents($morgen_raw)));
+	}
+	else {
 		echo (make_html(True, file_get_contents($morgen)));
+	}
 };
 
 
