bd6fa037e262c829354f8a93b9428f547fe7aadc
John Eberle <john@clusterag.leiningergymnasium.de>
2014-09-08 17:10:41 +0200

    added wipe

--- bin/wipe.php
+++ bin/wipe.php
@@ -5,7 +5,7 @@ if (login() && is_admin()) {
 	if ($_POST["wipe"]){
 		users_wipe();
 	}
-	echo(make_html(True, file_get_contents($wipe_admin_template_path)));
+	echo(make_html(True, file_get_contents($wipe_template_path)));
 }
 
 
