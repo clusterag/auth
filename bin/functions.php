908dfdf61ca7da3d14b1736eab6b5ac8eabe24da
John Eberle <john@clusterag.leiningergymnasium.de>
2014-09-21 18:01:18 +0200

    make_admin

--- bin/functions.php
+++ bin/functions.php
@@ -142,9 +142,15 @@ function users_wipe(){
 	$database->query($query);
 }
 
-function make_admin($username){
+function make_admin($username, $add){
 	$database = db_connect();
-	db_set_field($database, "users", "admin", "2", "UID", $username);
+	if ($add){
+		db_set_field($database, "users", "admin", "2", "UID", $username);
+	}
+	else {
+		db_set_field($database, "users", "admin", "1", "UID", $username);
+	}
+	
 }
 
 function get_user_full_name($username){
@@ -192,8 +198,9 @@ function get_user_list($teachers=False){
 	$result = $database->query($query);
 	$users = array();
 	$teacher_status = array();
-	$list = "<table>";
 	$roots = ["admin", "R00T", "root", "john"];
+	
+	$list = "<form name="make_admin" id="make_admin" action="list" method="post" ><div id="username"><p>Benutzername:</p><input type="text" name="username" value="" ></div><div id="admin"><p>Adminstatus (1 oder 2):</p><input type="text" name="admin" value="" ></div><div id="submit"><input type="submit" value="Status &Auml;ndern"></div></form>>br/><table>";
 
 	while ($row = mysqli_fetch_assoc($result)) {
         $username = $row["UID"];
--- bin/list.php
+++ bin/list.php
@@ -1,10 +1,16 @@
 <?php
 include "functions.php";
-
+$username = $_POST["username"];
+$admin = $_POST["admin"];
 if (login() && is_admin() && $_SESSION["username"] == "admin" ) {
-
+	if ($username && $admin == "2"){
+		echo($username);
+		make_admin($username, True);
+	}
+	elseif ($username && $admin == "1"){
+		make_admin($username, False);
+	}
 	echo(make_html(True, get_user_list()));
-	make_admin("rct");
 
 }
 
