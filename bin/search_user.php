2db8f2c13be66598b63de552cb44237cb0529af3
Valentin Wagner <vwagner@clusterag.leiningergymnasium.de>
2014-11-05 15:03:00 +0100

    search_user

--- bin/search_user.php
+++ bin/search_user.php
@@ -3,9 +3,7 @@ include "functions.php";
 $lastname = $_POST["lastname"];
 if (login() && is_admin()) {
 	if ($lastname){
-		echo "test123";
-		echo(make_html(True, search_user($lastname)));
-		echo "test321";
+		echo(make_html(True, file_get_contents($search_user_template_path) . search_user($lastname)));
 	}
 	else{
 		echo(make_html(True, file_get_contents($search_user_template_path)));
