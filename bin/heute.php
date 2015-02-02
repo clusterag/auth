9df6d488bc4752e15ea50dee4165d420ad9f7b8e
John Eberle <john@johnee.eu>
2015-02-02 14:44:20 +0100

    cleanup

--- bin/heute.php
+++ bin/heute.php
@@ -1,7 +1,6 @@
 <?php
 //CONF
 include "functions.php";
-
 //login must be the first function called because it sets the session cookie which must be done before the http body starts
 if (login()) {
 	if ($disable_python) {
@@ -11,6 +10,4 @@ if (login()) {
 		echo (make_html(True, file_get_contents($heute)));
 	}
 };
-
-
 ?>
