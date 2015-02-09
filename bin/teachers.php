2d63bfd9ee113286e2a2fb646be3df6141ea08e6
John Eberle <john@johnee.eu>
2015-02-09 13:37:16 +0100

    cleanup

--- bin/teachers.php
+++ bin/teachers.php
@@ -1,8 +1,6 @@
 <?php
 include "functions.php";
-
 if (login() && is_admin()) {
-
 	if ($_POST["username"] && $_POST["password"]){
 			add_user($_POST["firstname"], $_POST["lastname"], $_POST["username"], $_POST["password"], True);
 			echo(make_html(True, "Lehrer \"" . $_POST["username"] . "\" wurde erstellt" . file_get_contents($teachers_template_path)) . get_user_list(True));
@@ -13,8 +11,5 @@ if (login() && is_admin()) {
 		else {
 			echo(make_html(True, file_get_contents($teachers_template_path) . get_user_list(True)));
 	}
-
 }
-
-
 ?>
