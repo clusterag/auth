cb89e90a02437f547d5065d1ea6641ad1a320f83
John Eberle <john@johnee.eu>
2015-02-09 20:27:26 +0100

    Update conf.default.php

--- conf.default.php
+++ conf.default.php
@@ -10,6 +10,7 @@ $db_host = "db.host.tld";  //$dbhost
 $db_user = "dbuser1234";  //$dbuser
 $db_password = "dbpassword";  // passwort der Datenbank
 $db_database = "database1234";  // Name der Datenbank.
+//TODO: move this to the code
 $error_not_logged_in = "Benutzername oder Passwort sind falsch. Bitte versuchen Sie es erneut.";
 
 $heute = $root . "tables/heute.table";
