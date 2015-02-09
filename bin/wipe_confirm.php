867d888f7b009e755dea4a7f674f939605ba073f
John Eberle <john@johnee.eu>
2015-02-09 13:38:06 +0100

    Update wipe_confirm.php

--- bin/wipe_confirm.php
+++ bin/wipe_confirm.php
@@ -1,12 +1,9 @@
 <?php
 include "functions.php";
-
 if (login() && is_admin()) {
 	if ($_POST["wipe"]){
 		users_wipe();
 	}
 	echo(make_html(True, "Sind Sie wirklich sicher, dass sie ALLE Sch&uuml;erbenutzer l&ouml;schen wollen?" . file_get_contents($wipe_template_path)));
 }
-
-
 ?>
