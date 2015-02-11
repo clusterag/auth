b6f132ec67ed7c685f03e72ce31c1d34e07d730e
John Eberle <john@johnee.eu>
2015-02-11 15:25:55 +0100

    doc

--- bin/teachers.php
+++ bin/teachers.php
@@ -1,15 +1,20 @@
 <?php
 include "functions.php";
+//only accessible to admins
 if (login() && is_admin()) {
+	//if username and password have been posted, create new teacher account
+	//TODO: check if user exists
 	if ($_POST["username"] && $_POST["password"]){
 			add_user($_POST["firstname"], $_POST["lastname"], $_POST["username"], $_POST["password"], True);
 			echo(make_html(True, "Lehrer \"" . $_POST["username"] . "\" wurde erstellt" . file_get_contents($teachers_template_path)) . get_user_list(True));
 		}
-		elseif ($_POST["username"]) {
-			echo(make_html(True, "Bitte geben sie ein Passwort ein. " . file_get_contents($teachers_template_path)) . get_user_list(True));
+	//if no password has been posted, display error message
+	elseif ($_POST["username"]) {
+		echo(make_html(True, "Bitte geben sie ein Passwort ein. " . file_get_contents($teachers_template_path)) . get_user_list(True));
 		}
-		else {
-			echo(make_html(True, file_get_contents($teachers_template_path) . get_user_list(True)));
+	//if nothing has been posted display form
+	else {
+		echo(make_html(True, file_get_contents($teachers_template_path) . get_user_list(True)));
 	}
 }
 ?>
