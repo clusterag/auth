f8fc321b74d38761f0285b4950fffeb67ece3ae7
John Eberle <john@johnee.eu>
2015-02-11 15:21:49 +0100

    doc

--- bin/show_user.php
+++ bin/show_user.php
@@ -1,7 +1,10 @@
 <?php
 include "functions.php";
+//only accessible to admins
 if (login() && is_admin()) {
+	//if username has been posted and exists
 	if ($_POST["username"] && is_user($_POST["username"])){
+		//getting username from POST
 		$username = $_POST["username"];
 		echo(make_html(True, show_user($username) . file_get_contents($show_user_template_path)));
 	}
