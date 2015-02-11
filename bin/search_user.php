de6bf37299ee3cb51efab50129d9a0f0e06ffe6e
John Eberle <john@johnee.eu>
2015-02-11 15:10:54 +0100

    doc

--- bin/search_user.php
+++ bin/search_user.php
@@ -1,10 +1,14 @@
 <?php
 include "functions.php";
+//getting variables from post
 $lastname = $_POST["lastname"];
+//only accessible to admins
 if (login() && is_admin()) {
+	//if lastname has been posted look for the user
 	if ($lastname){
 		echo(make_html(True, file_get_contents($search_user_template_path) . search_user($lastname)));
 	}
+	//if lastname has not been posted display search form
 	else{
 		echo(make_html(True, file_get_contents($search_user_template_path)));
 	}
