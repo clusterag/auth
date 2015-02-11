487003eb95daaa2d02fbdf2f55cbfce150944932
John Eberle <john@johnee.eu>
2015-02-11 15:04:29 +0100

    doc

--- bin/heute.php
+++ bin/heute.php
@@ -3,6 +3,7 @@
 include "functions.php";
 //login must be the first function called because it sets the session cookie which must be done before the http body starts
 if (login()) {
+	//deadman switch
 	if ($disable_python) {
 		echo (make_html(True, file_get_contents($heute_raw)));
 	}
