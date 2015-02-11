d74fc3f82b90265b9bbe5097efe90b3d0eea0d10
John Eberle <john@johnee.eu>
2015-02-11 15:00:16 +0100

    doc

--- bin/change_user.php
+++ bin/change_user.php
@@ -1,16 +1,23 @@
 <?php
 include "functions.php";
+//only accessible to admins
 if (login() && is_admin()) {
+	//if no value has been posted echo user details
 	if ($_POST["username"] && is_user($_POST["username"]) && !$_POST["value"]){
+		//getting variables from POST
 		$username = $_POST["username"];
 		$parameter = $_POST["parameter"];
 		echo(make_html(True, change_user_template($username, $parameter)));
 	}
+	//if values have been posted 
 	elseif ($_POST["username"] && is_user($_POST["username"]) && $_POST["value"] && $_POST["parameter"]) {
+		//getting variables from POST
 		$username = $_POST["username"];
 		$parameter = $_POST["parameter"];
 		$value = $_POST["value"];
+		//make the changes
 		edit_user($username, $parameter, $value);
+		//show user details
 		echo(make_html(True, show_user($username)));
 	}
 }
