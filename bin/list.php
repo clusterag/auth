cc5778e9b6aae224ae488de9527987b40e2e11c4
John Eberle <john@johnee.eu>
2015-02-11 15:06:10 +0100

    doc

--- bin/list.php
+++ bin/list.php
@@ -1,14 +1,19 @@
 <?php
 include "functions.php";
+//getting variables from post
 $username = $_POST["username"];
 $admin = $_POST["admin"];
+//only accessible to user admin
 if (login() && is_admin() && $_SESSION["username"] == "admin" ) {
+	//remove admin status from posted username
 	if ($username && $admin == "2"){
 		echo($username);
 		make_admin($username, True);
 	}
+	//make posted username an admin
 	elseif ($username && $admin == "1"){
 		make_admin($username, False);
 	}
+	//echo full user list
 	echo(make_html(True, get_user_list()));
 }
