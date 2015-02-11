1d6525e4791b8e83a935874031b22e80428502fb
John Eberle <john@johnee.eu>
2015-02-11 15:07:44 +0100

    doc

--- bin/log.php
+++ bin/log.php
@@ -1,6 +1,6 @@
 <?php
 include "functions.php";
-
+//only accessible to user admin
 if (login() && is_admin() && $_SESSION["username"] == "admin" ) {
 	echo(make_html(True, file_get_contents($log_path)));
 }
